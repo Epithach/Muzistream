c2fc862e4323ab5bac442824bdd9565187b22464
Epithach <antoine.thach@gmail.com>
2016-10-18 16:17:00 +0200

    Adding Login page

---
+++ Muzistream-universal-app/Muzistream-universal-app/ViewModel/LoginViewModel.cs
@@ -0,0 +1,37 @@
+﻿using Muzistream.MVVM;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Muzistream_universal_app.ViewModel
+{
+    public class LoginViewModel : ViewModelBase
+    {
+        #region FIELDS
+
+
+
+        #endregion
+
+        #region PROPERTIES
+
+
+        #endregion
+
+        #region CONSTRUCTORS
+
+        public LoginViewModel()
+        {
+        }
+
+        #endregion
+
+        #region METHODS
+
+
+        #endregion
+    }
+
+}
